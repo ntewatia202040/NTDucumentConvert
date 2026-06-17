17c7b544ef9f217cb33573ea4e7bd40a44c78036
admin <admin@DESKTOP-UVS2HQD>
2026-06-17 10:14:26 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NTConvert.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
